7edb056d79643961e8a6142ef68cf74ebf9aca3c
karolinagudyka <u56121@student.wat.edu.pl>
2025-12-08 19:59:31 +0100

    feat: input of home component

--- config/node/frontend/my-app/src/pages/Home.js
+++ config/node/frontend/my-app/src/pages/Home.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import {Typography, Button} from "@mui/material";
 
 
 function Home(props) {
     return (
-        <div>strona startowa react</div>
+        <div className='none'>
+            <h1 className="home__title">GEOPORTAL</h1>
+            <Typography className="home__subtitle">
+                 Geoportal tematyczny poświęcony danym przestrzennym
+
+            </Typography>
+            <Button className="home__button" variant="contained" size="large">START</Button>
+
+
+        </div>
     );
 }
 
