f8d022583c2711689f37a095bb9dcfcde7ea3c5d
karolinagudyka <u56121@student.wat.edu.pl>
2025-12-08 20:42:20 +0100

    feat: fixed code and circular progress

--- config/node/frontend/my-app/src/App.js
+++ config/node/frontend/my-app/src/App.js
@@ -1,10 +1,24 @@
 import Home from "./pages/Home";
 import './styles/style.scss';
 
+import {Suspense} from "react";
+import {RouterProvider} from "react-router/dom";
+import {CircularProgress} from '@mui/material';
+import routes from "./routes/Router"
+
 function App() {
   return (
-    <div className="App">
-        <Home/>
+    <div className="app">
+        <Suspense
+            fallback={
+            <CircularProgress />
+             }
+        >
+            <RouterProvider router={routes}/>
+
+        </Suspense>
+
+
 
     </div>
   );
--- config/node/frontend/my-app/src/pages/Home.js
+++ config/node/frontend/my-app/src/pages/Home.js
@@ -4,7 +4,7 @@ import {Typography, Button} from "@mui/material";
 
 function Home(props) {
     return (
-        <div className='none'>
+        <div className='home'>
             <h1 className="home__title">GEOPORTAL</h1>
             <Typography className="home__subtitle">
                  Geoportal tematyczny poświęcony danym przestrzennym
