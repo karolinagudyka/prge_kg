2d1a570af509c75d2c7bfc079fe7052e7c1aa3ef
karolinagudyka <u56121@student.wat.edu.pl>
2025-12-10 08:12:15 +0100

    fix: createHashRouter instead of HashRouter

--- config/node/frontend/my-app/src/routes/Router.js
+++ config/node/frontend/my-app/src/routes/Router.js
@@ -1,8 +1,8 @@
-import {createBrowserRouter} from "react-router";
+import {createHashRouter} from "react-router";
 import {Home, About, Map, Services, ListOfItems} from './LazyImports';
 
 
-const routes = createBrowserRouter(
+const routes = createHashRouter(
     [
         {
             path: '/',
